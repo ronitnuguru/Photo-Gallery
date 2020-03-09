043ee69de9c47c77620287159bbc67e910322d46
Ronit Nuguru <ronit.nuguru@gmail.com>
2020-03-09 12:34:48 -0700

    configurable label, icon properties

--- force-app/main/default/lwc/photoGalleryCard/photoGalleryCard.js
+++ force-app/main/default/lwc/photoGalleryCard/photoGalleryCard.js
@@ -1,10 +1,30 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
+const defaultLabel = "Photo Gallery";
+const defaultIcon = "action:add_photo_video";
 
 export default class PhotoGalleryCard extends LightningElement {
 
     @api recordId;
+    @api minImages;
+    
+    @track cardLabel = defaultLabel;
+    @track cardIcon = defaultIcon;
+    
+    @api get label(){
+        return this.cardLabel;
+    }
+    set label(value) {
+        this.cardLabel = value || defaultLabel;
+    }
+
+    @api get icon(){
+        return this.cardIcon;
+    }
+    set icon(value) {
+        this.cardIcon = value || defaultIcon;
+    }
 
     get acceptedFormats() {
         return ['.jpg', '.jpeg', '.png', '.svg', '.gif'];
@@ -17,7 +37,6 @@ export default class PhotoGalleryCard extends LightningElement {
             message: `Image(s) have been uploaded`,
             variant: 'success',
         }));
-        
     }
 
 }
